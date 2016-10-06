a97fe2b045b6dc1bdfcdd52c24d44118a2d9f90b
André Arruda <andrearruda82@gmail.com>
2016-10-06 18:10:44 -0300

    first commit

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,4 @@
 <?php
 // Routes
 
-$app->get('/', function(){
-//    return $this->response->withRedirect($this->router->pathFor('instagram.login'));
-});
-
-
-$app->group('/instagram', function (){
-    $this->get('/feed/{username}[/{amount}]', 'App\Action\InstagramAction:feed')->setName('instagram.posts');
-});
+$app->get('/feed/{username}[/{amount}]', 'App\Action\InstagramAction:feed')->setName('instagram.posts');
