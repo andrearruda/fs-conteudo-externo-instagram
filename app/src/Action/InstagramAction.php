fee7f1854b67b12f470d6dda6098ce925f3d741e
André Arruda <andrearruda82@gmail.com>
2016-11-17 12:28:31 -0200

    ajuste p/ cache das midias

--- app/src/Action/InstagramAction.php
+++ app/src/Action/InstagramAction.php
@@ -38,84 +38,137 @@ final class InstagramAction
 
         if($data === false || $forceFileCached == true)
         {
-            $content = json_decode(file_get_contents($this->getInstagramMediaUrl()));
+            $content_json = json_decode(file_get_contents($this->getInstagramMediaUrl()));
+
+            $path_uploads = __DIR__ . '/../../../data/uploads/' . $this->getUsername() . '/';
+            if(!file_exists($path_uploads))
+            {
+                mkdir($path_uploads);
+            }
+
+            //CLEAR IMAGE
+            $files = scandir($path_uploads);
+            foreach ($files as $file)
+            {
+                $file_path = $path_uploads . $file;
+                if(is_file($file_path))
+                {
+                    $current_date = (new \DateTime('-10 days'));
+                    $file_date = (new \DateTime())->setTimestamp(filemtime($file_path));
+
+                    if($file_date < $current_date)
+                    {
+                        unlink($file_path);
+                    }
+                }
+            }
+
+            //IMAGE PROFILE
+            $img_profile = 'profile_picture_' . sha1($content_json->items[0]->user->profile_picture) . '.jpg';
+            $img_profile_path = $path_uploads . $img_profile;
+
+            if(!file_exists($img_profile_path))
+            {
+                $content = file_get_contents(str_replace('https://', 'http://', $content_json->items[0]->user->profile_picture));
+                file_put_contents($img_profile_path, $content);
+            }
 
             $data = array(
                 'info' => array(
-                    'id' => $content->items[0]->user->id,
-                    'name' => $content->items[0]->user->full_name,
-                    'username' => $content->items[0]->user->username,
-                    'midia' => array(
-                        'profile' => $content->items[0]->user->profile_picture,
+                    'date' => array(
+                        'created' => (new \DateTime())->format('Y-m-d H:i:s'),
+                    ),
+                    'name' => $content_json->items[0]->user->full_name,
+                    'username' => $content_json->items[0]->user->username,
+                    'media' => array(
+                        'profile' => sprintf('%s%s/%s', $this->getPaths()['upload_path_virtual'], $this->getUsername(), $img_profile),
                     )
                 ),
                 'feeds' => array()
             );
 
-            foreach($content->items as $i => $item)
+            foreach($content_json->items as $i => $item)
             {
+                //IMAGE FEED
+                $img_feed = 'feed_picture_' . sha1($item->images->standard_resolution->url) . '.jpg';
+                $img_feed_path = $path_uploads . $img_feed;
+
+                if(!file_exists($img_feed_path))
+                {
+                    $content = file_get_contents(str_replace('https://', 'http://', $item->images->standard_resolution->url));
+                    file_put_contents($img_feed_path, $content);
+                }
+
+                //VIDEO FEED
+                if($item->type == 'video')
+                {
+                    $video_feed = 'feed_video_' . sha1($item->videos->standard_resolution->url) . '.mp4';
+                    $video_feed_path = $path_uploads . $video_feed;
+
+                    if(!file_exists($video_feed_path))
+                    {
+                        $content = file_get_contents(str_replace('https://', 'http://', $item->videos->standard_resolution->url));
+                        file_put_contents($video_feed_path, $content);
+                    }
+
+                    $video_path_virtual = sprintf('%s%s/%s', $this->getPaths()['upload_path_virtual'], $this->getUsername(), $video_feed);
+                }
+                else
+                {
+                    $video_path_virtual = '';
+                }
+
                 $data['feeds'][$i] = array(
                     'type' => $item->type,
                     'date' => array(
                         'created' => date('Y-m-d H:i:s', $item->caption->created_time)
                     ),
                     'message' => array(
-                        'cut' => str_replace("\n", ' ', (string) Stringy::create($item->caption->text)->safeTruncate(250, '...')),
+                        'cut' => str_replace("\n", ' ', (string) Stringy::create($item->caption->text)->safeTruncate(140, '...')),
                         'full' => $item->caption->text
                     ),
-                    'midia' => array(
-                        'image' => $item->images->standard_resolution->url,
-                        'video' => $item->type == 'video' ? $item->videos->standard_resolution->url : ''
+                    'media' => array(
+                        'image' => sprintf('%s%s/%s', $this->getPaths()['upload_path_virtual'], $this->getUsername(), $img_feed),
+                        'video' => $video_path_virtual
                     ),
                     'engagement' => array(
                         'videoviews' => array(
-                            'total' => isset($item->video_views) ? $item->video_views : ''
+                            'total' => isset($item->video_views) ? $item->video_views : '0'
                         ),
                         'likes' => array(
                             'total' => $item->likes->count,
-                            'users' => array()
-                        ),
-                        'comments' => array(
-                            'total' => $item->comments->count,
                             'users' => array()
                         )
                     )
                 );
 
-                foreach($item->likes->data as $user)
+                foreach($item->likes->data as $j => $user)
                 {
+                    //IMAGE LIKE
+                    $img_like = 'like_picture_' . sha1($user->profile_picture) . '.jpg';
+                    $img_like_path = $path_uploads . $img_like;
+
+                    if(!file_exists($img_like_path))
+                    {
+                        $content = file_get_contents(str_replace('https://', 'http://', $user->profile_picture));
+                        file_put_contents($img_like_path, $content);
+                    }
+
                     $data['feeds'][$i]['engagement']['likes']['users'][] = array(
-                        'id' => $user->id,
                         'name' => $user->full_name,
                         'username' => $user->username,
-                        'midia' => array(
-                            'profile' => $user->profile_picture,
+                        'media' => array(
+                            'profile' => sprintf('%s%s/%s', $this->getPaths()['upload_path_virtual'], $this->getUsername(), $img_like)
                         )
                     );
+
+                    if($j >= 2)
+                    {
+                        break;
+                    }
                 }
 
-                foreach($item->comments->data as $comment)
-                {
-                    $data['feeds'][$i]['engagement']['comments']['users'][] = array(
-                        'date' => array(
-                            'created' => date('Y-m-d H:i:s', $comment->created_time)
-                        ),
-                        'message' => array(
-                            'cut' => str_replace("\n", ' ', (string) Stringy::create($comment->text)->safeTruncate(250, '...')),
-                            'full' => $comment->text
-                        ),
-                        'from' =>array(
-                            'id' => $comment->from->id,
-                            'name' => $comment->from->full_name,
-                            'username' => $comment->from->username,
-                            'midia' => array(
-                                'profile' => $comment->from->profile_picture,
-                            )
-                        )
-                    );
-                }
-
-                if($i > $this->getLength())
+                if($i >= ($this->getLength() - 1))
                 {
                     break;
                 }
